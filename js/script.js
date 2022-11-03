882ed8badc70b28c81e9f92d86693f2dc12e6d33
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2022-11-03 13:33:24 +0200

    added the ability to save text by pressing the button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 // Display current day and time at the top of the planer
 var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(currentDay);
+
+// Save task by clicking on button 
+$(".saveBtn").click(function() {
+    var text = $(".description").val()
+    console.log(text);
+});
